af679dbfbae3acbc3b4d4be43936609fb1f6d77d
Anastasija Milojević <amilojevic6802@gmail.com>
2026-08-27 02:55:01 +0200

    Create empty MyApp and app_setup

---
+++ app/include/MyApp.h
@@ -0,0 +1,16 @@
+//
+// Created by anastasija on 15.5.26..
+//
+
+#ifndef MATF_RG_PROJECT_MYAPP_H
+#define MATF_RG_PROJECT_MYAPP_H
+
+#include "engine/core/App.hpp"
+
+
+class MyApp : public engine::core::App {
+    void app_setup() override;
+};
+
+
+#endif //MATF_RG_PROJECT_MYAPP_H
---
+++ app/src/MyApp.cpp
@@ -0,0 +1,10 @@
+//
+// Created by anastasija on 15.5.26..
+//
+
+#include "MyApp.h"
+#include <spdlog/spdlog.h>
+
+void MyApp::app_setup() {
+    spdlog::info("Hello, setup!");
+}
